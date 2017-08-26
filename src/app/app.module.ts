49112d170e5b1c8d61335a1382aec33332132909
David Bell <dovib@msn.com>
2017-08-26 16:09:22 +1000

    Creation of questionnaire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './views/home/home.component';
+import { QuestionnaireComponent } from './views/questionnaire/questionnaire.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: '',   redirectTo: 'home', pathMatch: 'full' }
+  { path: '',   redirectTo: 'home', pathMatch: 'full' },
+  { path: 'questionnaire', component: QuestionnaireComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    QuestionnaireComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/views/questionnaire/questionnaire.component.ts
@@ -0,0 +1,17 @@
+import { ElementRef, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questionnaire',
+  templateUrl: './questionnaire.component.html',
+  styleUrls: ['./questionnaire.component.scss']
+})
+export class QuestionnaireComponent implements OnInit {
+
+  constructor(private element: ElementRef) { }
+
+  ngOnInit() {
+  	this.element.nativeElement.scrollIntoView();
+
+  }
+
+}
