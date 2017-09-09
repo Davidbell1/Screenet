56cfb27b7a32afae3959c56e529bb507018fd4d9
Alexandre NICOL <nemokervi@yahoo.fr>
2017-09-09 12:55:36 +1000

    Adding the first logic block to the website.

--- src/app/views/questionnaire/questionnaire.component.ts
+++ src/app/views/questionnaire/questionnaire.component.ts
@@ -1,4 +1,5 @@
 import { ElementRef, Component, OnInit } from '@angular/core';
+import { QUESTIONS } from '../../../assets/json/questions';
 
 @Component({
 	selector: 'app-questionnaire',
@@ -7,10 +8,31 @@ import { ElementRef, Component, OnInit } from '@angular/core';
 })
 
 export class QuestionnaireComponent implements OnInit {
+	question_id: number = 0;
+	questions: any = QUESTIONS;
+	results: any = {};
+
 	constructor(private element: ElementRef){}
 
 	ngOnInit() {
 		this.element.nativeElement.scrollIntoView();
+	}
 
+	save_answer( question_id, answer ){
+		this.results[question_id] = answer;
+
+		console.log( this. results );
+	}
+
+	next(){
+		if( this.questions[ this.question_id ].id && this.question_id < (this.questions.length - 1) ){
+			window.scrollTo(0, 0);
+			this.question_id = this.question_id + 1;
+		}
+	}
+
+	back(){
+		window.scrollTo(0, 0);
+		this.question_id = this.question_id - 1;
 	}
 }
---
+++ src/assets/json/questions.ts
@@ -0,0 +1,83 @@
+export const QUESTIONS =[
+	{
+		id: 'question_1',
+		title: 'Lorem ipsum dolor sit amet. 1',
+		subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate nesciunt quia sed explicabo alias, laboriosam et quam fugiat consectetur harum eos, commodi, quibusdam assumenda molestiae repudiandae minima doloribus tempore distinctio. 1',
+		answers: [
+			{
+				label: 'younger than 40',
+				value: '> 40'
+			},
+			{
+				label: '40 - 50',
+				value: '40 - 50'
+			},
+			{
+				label: '50 - 60',
+				value: '50 - 60'
+			},
+			{
+				label: '60 - 70',
+				value: '60 - 70'
+			},
+			{
+				label: 'older than 70',
+				value: '< 70'
+			},
+		]
+	},
+	{
+		id: 'question_2',
+		title: 'Lorem ipsum dolor sit amet. 2',
+		subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate nesciunt quia sed explicabo alias, laboriosam et quam fugiat consectetur harum eos, commodi, quibusdam assumenda molestiae repudiandae minima doloribus tempore distinctio. 2',
+		answers: [
+			{
+				label: 'younger than 40',
+				value: '> 40'
+			},
+			{
+				label: '40 - 50',
+				value: '40 - 50'
+			},
+			{
+				label: '50 - 60',
+				value: '50 - 60'
+			},
+			{
+				label: '60 - 70',
+				value: '60 - 70'
+			},
+			{
+				label: 'older than 70',
+				value: '< 70'
+			},
+		]
+	},
+	{
+		id: 'question_3',
+		title: 'Lorem ipsum dolor sit amet. 3',
+		subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate nesciunt quia sed explicabo alias, laboriosam et quam fugiat consectetur harum eos, commodi, quibusdam assumenda molestiae repudiandae minima doloribus tempore distinctio. 3',
+		answers: [
+			{
+				label: 'younger than 40',
+				value: '> 40'
+			},
+			{
+				label: '40 - 50',
+				value: '40 - 50'
+			},
+			{
+				label: '50 - 60',
+				value: '50 - 60'
+			},
+			{
+				label: '60 - 70',
+				value: '60 - 70'
+			},
+			{
+				label: 'older than 70',
+				value: '< 70'
+			},
+		]
+	},
+];
